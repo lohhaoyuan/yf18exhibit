0dca4856edc7d51f64c9444a4f17f7505190aa08
hao yuan <221409L@student.hci.edu.sg>
2025-05-22 14:59:29 +0800

    better tag

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -25,6 +25,27 @@ const badgeVariants = cva(
   },
 )
 
+const tagVariants = cva(
+  'inline-flex items-center justify-center rounded-md border px-48 py-12 text-xs font-medium w-full whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90',
+        secondary:
+          'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+        destructive:
+          'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/70',
+        outline:
+          'text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
 function Badge({
   className,
   variant,
@@ -43,4 +64,4 @@ function Badge({
   )
 }
 
-export { Badge, badgeVariants }
+export { Badge, badgeVariants, tagVariants }
